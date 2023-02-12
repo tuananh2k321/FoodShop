ace8720e9ea58ea8f35a91ddd41299abd9e5acc2
tuananh2k321 <111065838+tuananh2k321@users.noreply.github.com>
2023-02-12 17:25:27 +0700

    stack navigation =))

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from './screen/Welcome'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <Welcome/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Welcome" component={Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+
